f652d6f6ffd42883e3c7bc0e486b1858a406f2d0
GrantNima <1093536815@qq.com>
2019-10-16 18:12:05 +0800

    a-架构持续搭建

---
+++ src/main/java/com/grant/outsourcing/gs/config/MybatisConfig.java
@@ -0,0 +1,44 @@
+package com.grant.outsourcing.gs.config;
+
+import com.alibaba.druid.pool.DruidDataSource;
+import org.apache.ibatis.session.SqlSessionFactory;
+import org.mybatis.spring.SqlSessionFactoryBean;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import javax.sql.DataSource;
+
+/**
+ * Grant 2019年10月16日
+ */
+@Configuration
+public class MybatisConfig {
+
+    @Autowired
+    private DataSourceProperties dataSourceProperties;
+
+    @Bean(name = "dataSource")
+    public DataSource dataSource() {
+
+        DruidDataSource dataSource = new DruidDataSource();
+        dataSource.setUrl(dataSourceProperties.getUrl());
+        System.out.println(dataSourceProperties.getUrl());
+        dataSource.setDriverClassName(dataSourceProperties.getDriverClassName());
+        dataSource.setUsername(dataSourceProperties.getUsername());
+        dataSource.setPassword(dataSourceProperties.getPassword());
+        dataSource.setMaxActive(1000);
+        dataSource.setMaxIdle(20);
+        dataSource.setMaxWait(30000);
+
+        return dataSource;
+
+    }
+
+    public SqlSessionFactory sqlSessionFactory() throws Exception {
+        SqlSessionFactoryBean sqlSessionFactoryBean = new SqlSessionFactoryBean();
+        sqlSessionFactoryBean.setDataSource(dataSource());
+        return sqlSessionFactoryBean.getObject();
+    }
+}
