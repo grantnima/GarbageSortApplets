144b372b4a22043dda4cfc0fe9e9022b1f8a1489
GrantNima <1093536815@qq.com>
2019-10-17 09:06:41 +0800

    m-补充提交

--- src/main/java/com/grant/outsourcing/gs/config/MybatisConfig.java
+++ src/main/java/com/grant/outsourcing/gs/config/MybatisConfig.java
@@ -8,7 +8,6 @@ import org.springframework.boot.autoconfigure.jdbc.DataSourceProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
-import javax.annotation.Resource;
 import javax.sql.DataSource;
 
 /**
