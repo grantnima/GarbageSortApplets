f500c03455aee010415aa819d456f0e04e568702
GrantNima <1093536815@qq.com>
2020-01-10 18:21:22 +0800

    m-序列化配置

--- src/main/java/com/grant/outsourcing/gs/MainApplication.java
+++ src/main/java/com/grant/outsourcing/gs/MainApplication.java
@@ -42,7 +42,7 @@ public class MainApplication {
 
         //添加fastJson的配置信息 比如 ：是否要格式化返回的json数据
         FastJsonConfig fastJsonConfig = new FastJsonConfig();
-        fastJsonConfig.setSerializerFeatures(SerializerFeature.PrettyFormat, SerializerFeature.WriteMapNullValue);
+        fastJsonConfig.setSerializerFeatures(SerializerFeature.PrettyFormat, SerializerFeature.WriteMapNullValue,SerializerFeature.WriteNonStringValueAsString);
 
         //在转换器中添加配置信息
         fastConverter.setFastJsonConfig(fastJsonConfig);
