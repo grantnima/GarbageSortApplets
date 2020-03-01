f89b14f8d48d6a09c14278feaf96a66ef4de0027
GrantNima <1093536815@qq.com>
2020-03-01 17:38:23 +0800

    m-删除明文OSS配置；a-打赏人资料提交模块

--- src/main/java/com/grant/outsourcing/gs/constant/Constant.java
+++ src/main/java/com/grant/outsourcing/gs/constant/Constant.java
@@ -11,10 +11,4 @@ public interface Constant
 		int Others = 4;
 		int NormalLiquid = 5;
 	}
-
-	String OSS_ENDPOINT = "oss-cn-shenzhen.aliyuncs.com";
-	String OSS_ACCESSKEY_ID = "LTAI4FgmBGcAdgVjL8Gn2Rw9";
-	String OSS_ACCESSKEY_SECRET = "wwi6ePETPvo0kaUnLH84TyQsk12RW7";
-	String OSS_BUCKET_NAME = "yanluo-garbage-sort-b1";
-	String OSS_BUCKET_DOMAIN = "yanluo-garbage-sort-b1.oss-cn-shenzhen.aliyuncs.com";
 }
